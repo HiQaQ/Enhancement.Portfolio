0087c3c562c77061e14e553b4d4c91317e9d65f9
HiQaQ <56643250+HiQaQ@users.noreply.github.com>
2023-10-01 22:49:27 +0800

    Add files via upload

--- nkust.js
+++ nkust.js
@@ -1,43 +1,17 @@
-// 檢查輸入值
-function validateForm() {
+// 驗證登入資訊
+function validateLogin() {
+  // 取得表單資料
   const username = document.querySelector("input[name='username']").value;
   const password = document.querySelector("input[name='password']").value;
 
-  if (username === "" || password === "") {
-    alert("請輸入帳號和密碼");
-    return false;
-  }
+  // 檢查帳號和密碼是否正確
+  const isLoggedIn = username === "admin" && password === "123456";
 
-  return true;
+  // 如果登入成功，則導向首頁
+  if (isLoggedIn) {
+    window.location.href = "/";
+  }
 }
 
-// 處理登入請求
-function handleSubmit(event) {
-  event.preventDefault();
-
-  if (!validateForm()) {
-    return;
-  }
-
-  // 模擬登入
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "login.php");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({
-    username,
-    password,
-  }));
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // 登入成功
-      window.location.href = "/";
-    } else {
-      // 登入失敗
-      alert(xhr.responseText);
-    }
-  };
-}
-
-// 綁定事件
-document.querySelector("form").addEventListener("submit", handleSubmit);
+// 當表單提交時，執行驗證函數
+document.querySelector("form").addEventListener("submit", validateLogin);
