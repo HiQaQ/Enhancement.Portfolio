b71d4f1a9435faf6297738cc9f5418fb6fb7e6b2
HiQaQ <56643250+HiQaQ@users.noreply.github.com>
2023-10-01 22:46:44 +0800

    Add files via upload

--- nkust.js
+++ nkust.js
@@ -1,69 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { useState, useEffect } from "react";
-import { CSSTransition } from "react-transition-group";
+// 檢查輸入值
+function validateForm() {
+  const username = document.querySelector("input[name='username']").value;
+  const password = document.querySelector("input[name='password']").value;
 
-// 定義 UI 元件
-function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+  if (username === "" || password === "") {
+    alert("請輸入帳號和密碼");
+    return false;
+  }
 
-  // 登入按鈕
-  const handleLogin = async () => {
-    setLoading(true);
-    try {
-      // 模擬登入
-      await fetch("https://example.com/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-      });
-      setLoading(false);
-      // 登入成功
-      window.location.href = "/";
-    } catch (error) {
-      setLoading(false);
-      // 登入失敗
-      alert(error);
-    }
-  };
-
-  // 輸入框
-  const Input = ({ label, value, onChange }) => {
-    return (
-      <div className="input-container">
-        <label>{label}</label>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-          className="input"
-        />
-      </div>
-    );
-  };
-
-  // 主容器
-  return (
-    <div className="login-container">
-      <h1>登入</h1>
-      <Input label="帳號" value={username} onChange={setUsername} />
-      <Input label="密碼" value={password} onChange={setPassword} />
-      <button onClick={handleLogin} disabled={loading}>登入</button>
-      {loading && <CSSTransition in={loading} timeout={300} classNames="loading">
-        <div className="loading-container">
-          <div className="loading-spinner"></div>
-        </div>
-      </CSSTransition>}
-    </div>
-  );
+  return true;
 }
 
-// 渲染 UI
-ReactDOM.render(<Login />, document.querySelector("#root"));
+// 處理登入請求
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (!validateForm()) {
+    return;
+  }
+
+  // 模擬登入
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "login.php");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify({
+    username,
+    password,
+  }));
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // 登入成功
+      window.location.href = "/";
+    } else {
+      // 登入失敗
+      alert(xhr.responseText);
+    }
+  };
+}
+
+// 綁定事件
+document.querySelector("form").addEventListener("submit", handleSubmit);
