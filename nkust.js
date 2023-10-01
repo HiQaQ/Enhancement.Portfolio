dd722ab1ef626e836613514f64f6362991a6bfea
HiQaQ <56643250+HiQaQ@users.noreply.github.com>
2023-10-02 00:12:32 +0800

    Add files via upload

--- nkust.js
+++ nkust.js
@@ -1,17 +1,10 @@
-// 驗證登入資訊
-function validateLogin() {
-  // 取得表單資料
-  const username = document.querySelector("input[name='username']").value;
-  const password = document.querySelector("input[name='password']").value;
+var account = document.getElementById("account");
+var password = document.getElementById("password");
+const button = document.querySelector("button");
+button.addEventListener("click", ABC());
 
-  // 檢查帳號和密碼是否正確
-  const isLoggedIn = username === "a" && password === "1";
 
-  // 如果登入成功，則導向首頁
-  if (isLoggedIn) {
-    window.location.href = "https://www2.nkust.edu.tw/~c112110159";
-  }
+function ABC(){
+  document.getElementById("account").value="";
+  document.getElementById("password").value="";
 }
-
-// 當表單提交時，執行驗證函數
-document.querySelector("form").addEventListener("submit", validateLogin);
