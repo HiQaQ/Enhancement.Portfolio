60dd0d2e5c3251d9437a40a6ac7f13285759a5e1
HiQaQ <56643250+HiQaQ@users.noreply.github.com>
2024-09-20 02:33:15 +0800

    Add files via upload

--- home/script.js
+++ home/script.js
@@ -1,18 +1,13 @@
-// 檢查瀏覽器是否支援 history.pushState
 if (window.history && window.history.pushState) {
-    // 獲取當前網址路徑
-    const currentPath = window.location.pathname;
-  
-    // 檢查網址是否包含 'index.html'
-    if (currentPath.endsWith('/index.html')) {
-      // 使用 history.pushState 替換網址，移除 'index.html'
-      const newPath = currentPath.replace('/index.html', '/');
-      history.pushState({}, '', newPath);
-    }
-  
-    // 監聽 popstate 事件，防止返回時重新出現 'index.html'
-    window.addEventListener('popstate', function(event) {
-      event.preventDefault();
-      history.pushState({}, '', newPath);
-    });
+  const currentPath = window.location.pathname;
+
+  if (currentPath.endsWith("/index.html")) {
+    const newPath = currentPath.replace("/index.html", "/");
+    history.pushState({}, "", newPath);
   }
+
+  window.addEventListener("popstate", function (event) {
+    event.preventDefault();
+    history.pushState({}, "", newPath);
+  });
+}
