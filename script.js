7e9c1e527bb527a440d712288355e8fc68142851
HiQaQ <56643250+HiQaQ@users.noreply.github.com>
2024-09-19 19:55:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const welcomeText = document.getElementById("welcomeText");
+const words = ["Welcome", "To", "Enhancement Portfolio"];
+let currentWordIndex = 0;
+
+function showNextWord() {
+  if (currentWordIndex < words.length) {
+    welcomeText.textContent = words[currentWordIndex];
+    welcomeText.classList.add("show");
+
+    setTimeout(
+      () => {
+        welcomeText.classList.remove("show");
+
+        setTimeout(() => {
+          currentWordIndex++;
+          showNextWord();
+        }, 500);
+      },
+      currentWordIndex === words.length - 1 ? 1200 : 500
+    );
+  } else {
+    setTimeout(() => {
+      document.body.style.transition = "opacity 0.5s ease";
+      document.body.style.opacity = 0;
+
+      window.location.href = "home/index.html";
+    }, 100);
+  }
+}
+
+showNextWord();
